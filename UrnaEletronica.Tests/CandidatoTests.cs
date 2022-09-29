048aca2fa94b0255b85cf95f9608e009c4762925
Laís Fernandes Daniel <laisfd@hotmail.com>
2022-09-29 15:42:00 -0300

    More tests were made: - ValidarQuantidadeDeVotos_VotosIncorretosAposCadastro_RetornaFalso - ValidarQuantidadeDeVotos_VotosIncorretosAposNovoVoto_RetornaFalso - ValidarNomeDoCandidato_NomeCorreto_RetornaValorIgual - ValidarNomeDoCandidato_NomeIncorreto_RetornaExcecao

--- UrnaEletronica.Tests/CandidatoTests.cs
+++ UrnaEletronica.Tests/CandidatoTests.cs
@@ -20,6 +20,21 @@ namespace UrnaEletronica.Tests
                 .BeTrue();
         }
 
+        [Theory]
+        [InlineData(1)]
+        [InlineData(7897)]
+        [InlineData(555)]
+        [InlineData(-1)]
+        public void ValidarQuantidadeDeVotos_VotosIncorretosAposCadastro_RetornaFalso(int voto)
+        {
+            //Arrange + Act
+            var retorno = candidatoFake.ValidarQuantidadeDeVotos(voto);
+
+            //Assert
+            retorno.Should()
+                .BeFalse();
+        }
+
         [Fact]
         public void ValidarQuantidadeDeVotos_VotosCorretosAposNovoVoto_RetornaVerdadeiro()
         {
@@ -35,17 +50,46 @@ namespace UrnaEletronica.Tests
                 .BeTrue();
         }
 
-        [Fact]
-        public void ValidarNomeDoCandidato_NomeCorreto_RetornaVerdadeiro()
+        [Theory]
+        [InlineData(2)]
+        [InlineData(0)]
+        [InlineData(-1)]
+        [InlineData(500)]
+        public void ValidarQuantidadeDeVotos_VotosIncorretosAposNovoVoto_RetornaFalso(int voto)
         {
-            //Arrange
-            var nome = "Fulana";
+            //Arrange + Act
+            candidatoFake.AdicionarVoto();
+            var retorno = candidatoFake.ValidarQuantidadeDeVotos(voto);
 
-            //Act
+            //Assert
+            retorno.Should()
+                .BeFalse();
+        }
+
+        [Theory]
+        [InlineData("Fulana")]
+        [InlineData("fulana")]
+        [InlineData("FULANA")]
+        public void ValidarNomeDoCandidato_NomeCorreto_RetornaValorIgual(string nome)
+        {
+            //Arrange + Act
             var retorno = candidatoFake.ValidarNomeDoCandidato(nome);
 
             //Assert
             Assert.Equal(nome, retorno);
+        }
+
+        [Theory]
+        [InlineData("Ciclana")]
+        [InlineData("Fulano")]
+        [InlineData("")]
+        public void ValidarNomeDoCandidato_NomeIncorreto_RetornaExcecao(string nome)
+        {
+            //Arrange + Act
+            Action act = () => candidatoFake.ValidarNomeDoCandidato(nome);
+
+            //Assert
+            act.Should().Throw<Exception>().WithMessage("O nome do candidato não está correto.");
         }
     }
 }
